72646052d523e93a729188fcec10be85e5a77ffc
Shaun McCormick <splittingred@gmail.com>
2021-12-20 11:51:33 -0600

    Various bundle / other updates

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2017_08_10_220531) do
 
-  create_table "products", charset: "utf8", force: :cascade do |t|
+  create_table "products", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.datetime "created_at", null: false
